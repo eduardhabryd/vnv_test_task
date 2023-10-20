696a5012fc7dfba765d5c88653ad9ede6494d183
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-21 00:52:55 +0300

    made CustomGroup to have only one custom field: description

--- app/models.py
+++ app/models.py
@@ -3,23 +3,21 @@ from django.db import models
 
 
 class CustomGroup(Group):
-	data_analytics = models.BooleanField(default=False)
-	services_analytics = models.BooleanField(default=False)
-	voice_analytics = models.BooleanField(default=False)
-	queue_stats = models.BooleanField(default=False)
-	voice_stats = models.BooleanField(default=False)
-	video = models.BooleanField(default=False)
-	smart_access = models.BooleanField(default=False)
-	diagrams = models.BooleanField(default=False)
+	description = models.TextField(blank=True)
 	
 	class Meta:
 		verbose_name = 'Custom Group'
 		verbose_name_plural = 'Custom Groups'
 
 
-class User(AbstractUser):
-	group = models.ForeignKey(
-		CustomGroup, blank=True, related_name="users", null=True, on_delete=models.SET_NULL
+class CustomUser(models.Model):
+	username = models.CharField(
+		max_length=30,
+		unique=True
 	)
+	group = models.ForeignKey(
+		CustomGroup, blank=True, related_name="users", null=True, on_delete=models.PROTECT
+	)
+	created = models.DateTimeField(auto_now_add=True)
 
 
