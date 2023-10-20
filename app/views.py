853dda64607d2a46cceed3f7963191b6aa85bb54
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-20 23:59:06 +0300

    implement UserCreateView and UserUpdateView

--- app/views.py
+++ app/views.py
@@ -1,12 +1,45 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Group
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
+
+from .forms import UserCreationCustomForm, UserUpdateForm
+from .models import CustomGroup
 
 
 class UserListView(generic.ListView):
 	model = get_user_model()
 
 
+class UserCreateView(generic.CreateView):
+	model = get_user_model()
+	form_class = UserCreationCustomForm
+	success_url = reverse_lazy("app:user-list")
+
+
+class UserUpdateView(generic.UpdateView):
+	model = get_user_model()
+	form_class = UserUpdateForm
+	success_url = reverse_lazy("app:user-list")
+
+
 class GroupListView(generic.ListView):
-	model = Group
+	model = CustomGroup
+
+
+
+# class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
+#     model = Manufacturer
+#     fields = "__all__"
+#     success_url = reverse_lazy("taxi:manufacturer-list")
+#
+#
+# class ManufacturerUpdateView(LoginRequiredMixin, generic.UpdateView):
+#     model = Manufacturer
+#     fields = "__all__"
+#     success_url = reverse_lazy("taxi:manufacturer-list")
+#
+#
+# class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
+#     model = Manufacturer
+#     success_url = reverse_lazy("taxi:manufacturer-list")
