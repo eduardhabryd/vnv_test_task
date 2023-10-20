aae13e26dd5d2ffccc16cd1a03bf8326402cd9e7
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-20 22:14:31 +0300

    implement CustomGroup model and User(AbstractUser) model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
 
-# Create your models here.
+
+class CustomGroup(Group):
+	description = models.TextField(blank=True)
+	
+	class Meta:
+		verbose_name = 'Custom Group'
+		verbose_name_plural = 'Custom Groups'
+
+
+class User(AbstractUser):
+	groups = models.ManyToManyField(
+		CustomGroup, blank=True, related_name="users"
+	)
+
+
