ed5022104e49a4c5c7ed4def57d9ebdb972fd852
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-20 22:17:02 +0300

    implement UserListView and GroupListView

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class UserListView(generic.ListView):
+	model = get_user_model()
+
+
+class GroupListView(generic.ListView):
+	model = Group
