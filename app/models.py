f834422634900469daa3ce094cc08b93e1042f82
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-21 01:16:43 +0300

    blacked

--- app/models.py
+++ app/models.py
@@ -1,23 +1,22 @@
-from django.contrib.auth.models import AbstractUser, Group
+from django.contrib.auth.models import Group
 from django.db import models
 
 
 class CustomGroup(Group):
-	description = models.TextField(blank=True)
-	
-	class Meta:
-		verbose_name = 'Custom Group'
-		verbose_name_plural = 'Custom Groups'
+    description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name = "Custom Group"
+        verbose_name_plural = "Custom Groups"
 
 
 class CustomUser(models.Model):
-	username = models.CharField(
-		max_length=30,
-		unique=True
-	)
-	group = models.ForeignKey(
-		CustomGroup, blank=True, related_name="users", null=True, on_delete=models.PROTECT
-	)
-	created = models.DateTimeField(auto_now_add=True)
-
-
+    username = models.CharField(max_length=30, unique=True)
+    group = models.ForeignKey(
+        CustomGroup,
+        blank=True,
+        related_name="users",
+        null=True,
+        on_delete=models.PROTECT,
+    )
+    created = models.DateTimeField(auto_now_add=True)
--- app/views.py
+++ app/views.py
@@ -5,46 +5,48 @@ from .models import CustomGroup, CustomUser
 
 
 class UserListView(generic.ListView):
-	model = CustomUser
+    model = CustomUser
 
 
 class UserCreateView(generic.CreateView):
-	model = CustomUser
-	fields = "__all__"
-	success_url = reverse_lazy("app:user-list")
+    model = CustomUser
+    fields = "__all__"
+    success_url = reverse_lazy("app:user-list")
 
 
 class UserUpdateView(generic.UpdateView):
-	model = CustomUser
-	fields = "__all__"
-	success_url = reverse_lazy("app:user-list")
+    model = CustomUser
+    fields = "__all__"
+    success_url = reverse_lazy("app:user-list")
 
 
 class UserDeleteView(generic.DeleteView):
-	model = CustomUser
-	success_url = reverse_lazy("app:user-list")
+    model = CustomUser
+    success_url = reverse_lazy("app:user-list")
 
 
 class GroupListView(generic.ListView):
-	model = CustomGroup
+    model = CustomGroup
 
 
 class GroupCreateView(generic.CreateView):
-	model = CustomGroup
-	success_url = reverse_lazy("app:group-list")
-	fields = (
-		"name", "description",
-	)
+    model = CustomGroup
+    success_url = reverse_lazy("app:group-list")
+    fields = (
+        "name",
+        "description",
+    )
 
 
 class GroupUpdateView(generic.UpdateView):
-	model = CustomGroup
-	success_url = reverse_lazy("app:group-list")
-	fields = (
-		"name", "description",
-	)
+    model = CustomGroup
+    success_url = reverse_lazy("app:group-list")
+    fields = (
+        "name",
+        "description",
+    )
 
 
 class GroupDeleteView(generic.DeleteView):
-	model = CustomGroup
-	success_url = reverse_lazy("app:group-list")
+    model = CustomGroup
+    success_url = reverse_lazy("app:group-list")
