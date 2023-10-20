5d96a4db1e02c23c74c54fea476e4cfbf247ebd5
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-20 23:57:27 +0300

    add path for users/create/ and for users/<int:pk>/

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import UserListView, GroupListView
+from .views import UserListView, UserCreateView, UserUpdateView, GroupListView
 
 urlpatterns = [
     path("users/", UserListView.as_view(), name="user-list"),
-    # path("users/create/", admin.site.urls),
+    path("users/create/", UserCreateView.as_view(), name="user-create"),
+    path("users/<int:pk>/", UserUpdateView.as_view(), name="user-update"),
     path("groups/", GroupListView.as_view(), name="group-list"),
 ]
 
