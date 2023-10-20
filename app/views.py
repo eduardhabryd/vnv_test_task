3ad102d894d2096499c2c8f3859c9b1e14aef623
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-21 00:55:10 +0300

    simplified views, make them use CustomUser where previous was get_user_model

--- app/views.py
+++ app/views.py
@@ -1,25 +1,27 @@
-from django.contrib.auth import get_user_model
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from .forms import UserCreationCustomForm, UserUpdateForm
-from .models import CustomGroup
+from .models import CustomGroup, CustomUser
 
 
 class UserListView(generic.ListView):
-	model = get_user_model()
+	model = CustomUser
 
 
 class UserCreateView(generic.CreateView):
-	model = get_user_model()
-	form_class = UserCreationCustomForm
+	model = CustomUser
+	fields = "__all__"
 	success_url = reverse_lazy("app:user-list")
 
 
 class UserUpdateView(generic.UpdateView):
-	model = get_user_model()
-	form_class = UserUpdateForm
+	model = CustomUser
+	fields = "__all__"
+	success_url = reverse_lazy("app:user-list")
+
+
+class UserDeleteView(generic.DeleteView):
+	model = CustomUser
 	success_url = reverse_lazy("app:user-list")
 
 
@@ -27,19 +29,22 @@ class GroupListView(generic.ListView):
 	model = CustomGroup
 
 
+class GroupCreateView(generic.CreateView):
+	model = CustomGroup
+	success_url = reverse_lazy("app:group-list")
+	fields = (
+		"name", "description",
+	)
 
-# class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
-#     model = Manufacturer
-#     fields = "__all__"
-#     success_url = reverse_lazy("taxi:manufacturer-list")
-#
-#
-# class ManufacturerUpdateView(LoginRequiredMixin, generic.UpdateView):
-#     model = Manufacturer
-#     fields = "__all__"
-#     success_url = reverse_lazy("taxi:manufacturer-list")
-#
-#
-# class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
-#     model = Manufacturer
-#     success_url = reverse_lazy("taxi:manufacturer-list")
+
+class GroupUpdateView(generic.UpdateView):
+	model = CustomGroup
+	success_url = reverse_lazy("app:group-list")
+	fields = (
+		"name", "description",
+	)
+
+
+class GroupDeleteView(generic.DeleteView):
+	model = CustomGroup
+	success_url = reverse_lazy("app:group-list")
